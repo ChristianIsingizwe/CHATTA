4d3577a31b3335a4231d41ca905c4d37ea592f90
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-01-06 17:18:06 +0200

    Some changes made

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,23 @@
 import Joi from "joi";
 import User from "../models/userModel";
-import { hashPassword } from "../utils/passwordHelper";
+import { hashPassword, verifyPassword } from "../utils/passwordHelper";
 import {
   generateAccessToken,
   generateRefreshToken,
 } from "../utils/generateTokens";
 
-const userSchema = Joi.object({
+const userRegisterSchema = Joi.object({
   name: Joi.string().min(3).max(100).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .min(8)
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    )
+    .required(),
+});
+
+const userSignInSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string()
     .min(8)
@@ -19,7 +29,7 @@ const userSchema = Joi.object({
 
 const registerUser = async (req, res) => {
   try {
-    const { error, value } = userSchema.validate(req.body, {
+    const { error, value } = userRegisterSchema.validate(req.body, {
       abortEarly: false,
     });
     if (error) {
@@ -55,6 +65,43 @@ const registerUser = async (req, res) => {
   }
 };
 
-// const signInUser = async (req, res)=>{
+const signInUser = async (req, res) => {
+  try {
+    const { error, value } = userSignInSchema.validate(req.body, {
+      abortEarly: false,
+    });
+    if (error) {
+      const errorMessages = error.details.map((detail) => detail.message);
+      return res.status(400).json({ errors: errorMessages });
+    }
 
-// }
+    const { email, password } = value;
+    const user = User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordCorrect = verifyPassword(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const accessToken = generateAccessToken(user._id);
+    const refreshToken = generateRefreshToken(user._id, user.tokenVersion);
+
+    res
+      .status(200)
+      .json({ name: user.name, email: user.email, accessToken, refreshToken });
+  } catch (error) {
+    console.error("An error occurred: ", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
+
+const findUsers = async (req, res)=>{
+
+}
+
+const findUser = async (req, res)=>{
+    
+}
