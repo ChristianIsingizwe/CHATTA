b6a56cdc655aa794da287feed5ae4873d0a36314
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-01-11 14:50:58 +0200

    Some changes made

--- frontend/src/compo/sidebar/LogoutButton.jsx
+++ frontend/src/compo/sidebar/LogoutButton.jsx
@@ -1,7 +1,10 @@
+import {BiLogOut} from 'react-icons/bi'
 
 function LogoutButton() {
   return (
-    <div>LogoutButton</div>
+    <div className="mt-auto">
+        <BiLogOut className ="w-6 h-6 text-white cursor-pointer" />
+    </div>
   )
 }
 
