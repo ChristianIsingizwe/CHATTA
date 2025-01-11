8a350e3359d68ac3544b18959797042866de3d54
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-01-11 15:36:37 +0200

    Some changes made

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import Sidebar from "../../compo/sidebar/Sidebar";
+import MessageContainer from "../../compo/messages/MessageContainer";
 function Home() {
   return (
     <div className="flex sm:h-[450px] md:-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
-      {/* <MessageContainer /> */}
+      <MessageContainer />
     </div>
   );
 }
