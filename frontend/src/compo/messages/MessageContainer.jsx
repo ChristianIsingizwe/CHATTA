5c4fed094402b2e262800b4b8cc348026c24acbc
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-01-12 03:43:12 +0200

    Some changes made

--- frontend/src/compo/messages/MessageContainer.jsx
+++ frontend/src/compo/messages/MessageContainer.jsx
@@ -1,23 +1,28 @@
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
+import { TiMessages } from "react-icons/ti";
 
 function MessageContainer() {
-    const noChatSelected = true;
+  const noChatSelected = true;
 
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      <>
-        {/* Header */}
+      {noChatSelected ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          {/* Header */}
 
-        <div className="bg-slate-500 px-4 py-2 mb-2">
-          <span className="label-text">To:</span>{" "}
-          <span className="text-gray-900 font-bold">John Doe</span>
-        </div>
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">To:</span>{" "}
+            <span className="text-gray-900 font-bold">John Doe</span>
+          </div>
 
-        {/* Messages */}
-        <Messages />
-        <MessageInput />
-      </>
+          {/* Messages */}
+          <Messages />
+          <MessageInput />
+        </>
+      )}
     </div>
   );
 }
